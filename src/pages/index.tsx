6918168b94c328a591bc77642c82f45b55b0302f
alyssondepaula <alyssonjuniordepaula@gmail.com>
2021-10-01 23:50:36 -0300

    style: main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import { CardEmpresa } from '../components/CardEmpresa'
 import { MenuBar } from '../components/MenuBar'
 import { RightBar } from '../components/RightBar'
-import {Container, MainContent} from './styles'
+import {Container, HeadingPageContainer, Icon, InputContainer, MainContent, Text, IconContainer, Input, ChartContainer, RecentCompaniesContainer} from './styles'
 
 const Home: NextPage = () => {
   return (<>
@@ -10,9 +10,27 @@ const Home: NextPage = () => {
     <MenuBar/>
       <MainContent>
 
-        <CardEmpresa/>
+        <HeadingPageContainer>
+          <Icon src='/images/icon-dashboard.png' alt='Logo Dashboard' loading='lazy'/>
+          <Text>Dashboard</Text>
+        </HeadingPageContainer>
+
+        <InputContainer>
+           <Input/>
+          <IconContainer> 
+          <Icon src='/images/search.png' alt='Search Icon' loading='lazy'/>
+          </IconContainer>
+        </InputContainer>
+
+
+        <ChartContainer/>
+
+
+
+          <RecentCompaniesContainer>
+
+          </RecentCompaniesContainer>
       </MainContent>
-     
      <RightBar/>
      </Container>
     </>
--- src/pages/styles.tsx
+++ src/pages/styles.tsx
@@ -15,4 +15,79 @@ export const MainContent = styled.div`
    flex-direction: column;
    border-top-left-radius: 1.5rem;
    border-bottom-left-radius: 1.5rem;
+   padding: 2rem 1.31rem 4rem 12px;
+`;
+
+export const HeadingPageContainer = styled.div`
+   height: 1.75rem;
+   display: inline-flex;
+   align-items: center;
+   justify-content: space-between;
+`;
+
+
+export const Text = styled.h2`
+   color: var(--color-gray003);
+   font-size: 1.75rem;
+`;
+
+export const Icon = styled.img`
+   width: 1.5rem;
+   height: 1.5rem;
+`;
+
+export const InputContainer = styled.div`
+   display: flex;
+   width: 22.75rem;
+   height: 2.5rem;
+   background-color: var(--color-white);
+   margin-top: 2rem;
+   border-radius: .5rem;
+   border-top:2px solid #E1E0E7;
+   border-left:2px solid #E1E0E7;
+   border-bottom:2px solid #E1E0E7;
+   flex-direction: row;
+   align-items: center;
+   justify-content: space-between;
+`;
+
+export const IconContainer = styled.div`
+   display: flex;
+   width: 2.5rem;
+   height: 2.5rem;
+   background-color: var(--color-primary);
+   border-radius: .5rem;
+   align-items: center;
+   justify-content: center;
+
+`;
+
+export const Input = styled.input`
+  flex: 1;
+  height: 100%;
+  padding-left: 1rem;
+  border: none;
+  outline: none;
+`;
+
+export const ChartContainer = styled.div`
+   display: flex;
+   width: 100%;
+   height: 23.75rem;
+   background-color: var(--color-white);
+   border-radius: .5rem;
+   align-items: center;
+   justify-content: center;
+   margin-top: 1.5rem;
+`;
+
+export const RecentCompaniesContainer = styled.div`
+   display: flex;
+   width: 100%;
+   height: 8.5rem;
+   background-color: var(--color-white);
+   border-radius: .5rem;
+   align-items: center;
+   justify-content: center;
+   margin-top: 2.37rem;
 `;
